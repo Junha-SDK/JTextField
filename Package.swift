9076cf78cd41c083c7d40fa75faa3407b584dd9e
jjunhaa0211 <goodjunha@gmail.com>
2024-08-07 09:01:25 +0900

    커밋

--- Package.swift
+++ Package.swift
@@ -11,11 +11,26 @@ let package = Package(
             name: "JTextField",
             targets: ["JTextField"]),
     ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // Here we're depending on three external packages.
+        .package(url: "https://github.com/Junha-SDK/JUtile", from: "0.0.2"),
+        .package(url: "https://github.com/devxoul/Then", from: "3.0.0"),
+        .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.0.1"),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0"))
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "JTextField"),
+            name: "JTextField",
+            dependencies: [
+                "JUtile",
+                "Then",
+                "SnapKit",
+                .product(name: "RxSwift", package: "RxSwift"),
+                .product(name: "RxCocoa", package: "RxSwift")
+            ],
+            path: "Sources/JTextField"
+        ),
         .testTarget(
             name: "JTextFieldTests",
             dependencies: ["JTextField"]),
--- Sources/JTextField/JTextField.swift
+++ Sources/JTextField/JTextField.swift
@@ -1,2 +1,126 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import UIKit
+import Then
+import SnapKit
+import RxCocoa
+import RxSwift
+
+open class JTextField: UITextField {
+
+    private let disposeBag = DisposeBag()
+
+    public var errorFont: UIFont?
+    public var underlineColor: UIColor
+    public var errorColor: UIColor
+    public var focusUnderlineColor: UIColor
+    public var animationDuration: TimeInterval
+
+    public let underlineView = UIView()
+    public let errorLabel = UILabel()
+    public let deleteButton = UIButton(type: .custom)
+
+    public var showImage: Bool = false {
+        didSet {
+            deleteButton.isHidden = !showImage
+        }
+    }
+
+    public var showError: Bool = false {
+        didSet {
+            errorLabel.isHidden = !showError
+            underlineView.backgroundColor = showError ? errorColor : underlineColor
+        }
+    }
+
+    public var errorMessage: String? {
+        didSet {
+            errorLabel.text = errorMessage
+        }
+    }
+
+    public override var placeholder: String? {
+        didSet {
+            super.placeholder = placeholder
+        }
+    }
+
+    public init(
+        frame: CGRect = .zero,
+        errorFont: UIFont = UIFont.systemFont(ofSize: 12),
+        underlineColor: UIColor = .gray,
+        errorColor: UIColor = .red,
+        focusUnderlineColor: UIColor = .blue,
+        animationDuration: TimeInterval = 0.3
+    ) {
+        self.errorFont = errorFont
+        self.underlineColor = underlineColor
+        self.errorColor = errorColor
+        self.focusUnderlineColor = focusUnderlineColor
+        self.animationDuration = animationDuration
+        super.init(frame: frame)
+        setupUI()
+    }
+
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupUI() {
+        configure()
+        bind()
+
+        self.tintColor = .black
+        errorLabel.font = errorFont
+        underlineView.backgroundColor = underlineColor
+        errorLabel.textColor = errorColor
+        errorLabel.isHidden = true
+
+        if #available(iOS 13.0, *) {
+            deleteButton.setImage(UIImage(systemName: "xmark.circle"), for: .normal)
+        } else {
+            print("13 이하는 아직 지원하지 않습니다.")
+        }
+        deleteButton.isHidden = true
+    }
+
+    private func configure() {
+        [underlineView, errorLabel, deleteButton].forEach { self.addSubview($0) }
+
+        underlineView.snp.makeConstraints {
+            $0.leading.trailing.bottom.equalToSuperview()
+            $0.height.equalTo(1)
+        }
+
+        errorLabel.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+            $0.top.equalTo(underlineView.snp.bottom).offset(8)
+        }
+
+        deleteButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(10)
+            $0.centerY.equalToSuperview()
+            $0.width.height.equalTo(24)
+        }
+    }
+
+    private func bind() {
+        deleteButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                self?.text = ""
+                self?.deleteButton.isHidden = true
+            })
+            .disposed(by: disposeBag)
+
+        self.rx.controlEvent(.editingDidBegin).asObservable().subscribe(onNext: { [weak self] _ in
+            UIView.animate(withDuration: self?.animationDuration ?? 0.3) {
+                self?.underlineView.backgroundColor = self?.focusUnderlineColor
+            }
+        }).disposed(by: disposeBag)
+
+        self.rx.controlEvent(.editingDidEnd).asObservable().subscribe(onNext: { [weak self] _ in
+            UIView.animate(withDuration: self?.animationDuration ?? 0.3) {
+                self?.underlineView.backgroundColor = self?.text?.isEmpty ?? true ? self?.underlineColor : self?.focusUnderlineColor
+            }
+            self?.deleteButton.isHidden = self?.text?.isEmpty ?? true
+        }).disposed(by: disposeBag)
+    }
+}
